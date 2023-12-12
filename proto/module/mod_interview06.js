a7197ed0802444605aac5fd0e9235e9e927366f9
김은영 <66293497+aldo814@users.noreply.github.com>
2023-12-12 10:06:10 +0900

    네오바이오텍

--- proto/module/mod_interview06.js
+++ proto/module/mod_interview06.js
@@ -1,22 +1,25 @@
 interview06 = $('.mod_interview06 .interview');
-function interview06_slide(){
-    interview06.slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: false,
-        dots: true,
-        arrows: false,
-        infinite: false,
-        adaptiveHeight: false,
-    });
-}
-interview06_slide();
 
-$(".mod_interview06 .bg .q").click(function() {
-    var $this = $(this);
-    var $parent = $this.parent().siblings();
-    $parent.find(".q").removeClass("active");
-    $this.addClass("active");
-    interview06.slick('unslick');
-    interview06_slide();
-});
+            function interview06_slide() {
+                interview06.slick({
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: false,
+                    dots: false,
+                    prevArrow:'.mod_interview06 .prev_arrow',
+                    nextArrow:'.mod_interview06 .next_arrow',
+                    arrows: true,
+                    infinite: true,
+                    adaptiveHeight: false,
+                });
+            }
+            interview06_slide();
+
+            $(".mod_interview06 .bg .q").click(function() {
+                var $this = $(this);
+                var $parent = $this.parent().siblings();
+                $parent.find(".q").removeClass("active");
+                $this.addClass("active");
+                interview06.slick('unslick');
+                interview06_slide();
+            });
